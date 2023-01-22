86e81de523eebf3be001d509a9176a1bc492aaf2
Yuesef49 <yusufbot3@gmail.com>
2023-01-22 16:20:33 +0100

    dfghj

--- track.js
+++ track.js
@@ -10,15 +10,13 @@ function tracker(){
     const elem = document.getElementById("body1");
     elem.addEventListener("click", countMouseClick);
     
+    incrSec();
     
-    let second = 0;
+}
+let second = 0;
     function incrSec(){
         second = second + 1;
         var timeSpentString = new String("Total time spent: " + second); 
         document.getElementById("trackerInside2").innerHTML = timeSpentString;
         setTimeout(incrSec, 1000);
     }
-    
-    
-    
-}
