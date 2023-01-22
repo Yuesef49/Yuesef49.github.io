6aa078dd14c914c3cb6c8d86f43860f9f8d3c3f4
Yuesef49 <yusufbot3@gmail.com>
2023-01-22 17:44:28 +0100

    key press update

--- track.js
+++ track.js
@@ -1,5 +1,6 @@
 function tracker(){
     let a = 0;
+    let keyPressAmount = 0;
 
     function countMouseClick() {
         a = a + 1;
@@ -9,14 +10,32 @@ function tracker(){
     
     const elem = document.getElementById("body1");
     elem.addEventListener("click", countMouseClick);
-    
+    elem.addEventListener("keydown", countKeyPress(keyPressAmount));
+    var test = "<p id='trackerInside3'></p>";
+    document.getElementById("tracker").insertAdjacentHTML('beforeend',test);
     incrSec();
     
 }
-let second = 0;
+let seconds = 0;
+let minutes = 0;
     function incrSec(){
-        second = second + 1;
-        var timeSpentString = new String("Total time spent: " + second); 
+        var timeSpentString = new String(); 
+        seconds = seconds + 1;
+        if(seconds >= 60){
+            seconds = seconds % 60;
+            minutes = minutes + 1;
+        }
+        if(minutes > 0){
+            timeSpentString = "Total time spent: "+minutes+" minutes and "+seconds+" seconds";
+        }else{
+            timeSpentString = "Total time spent: "+seconds+" seconds";
+        }
         document.getElementById("trackerInside2").innerHTML = timeSpentString;
         setTimeout(incrSec, 1000);
+    }
+
+    function countKeyPress(keyPressAmount){
+        keyPressAmount++;
+        var keyPressString = new String("Total key presses: " +keyPressAmount);
+        document.getElementById("trackerInside3").innerHTML = keyPressString;
     }
