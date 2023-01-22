090a81c377906cca4e24c013bdaae243539060d1
Yuesef49 <yusufbot3@gmail.com>
2023-01-22 16:15:10 +0100

    s

--- track.js
+++ track.js
@@ -1,28 +1,24 @@
-let a = 0;
+function tracker(){
+    let a = 0;
 
-function countMouseClick() {
-    a = a + 1;
-     document.getElementById("trackerInside").innerHTML =
-     "Number of mouse clicks:" + a;
-}
-
-const elem = document.getElementById("body1");
-elem.addEventListener("click", countMouseClick);
-
-document.getElementById("signUpSubmitButton").addEventListener("click", incrementionStopper);
-
-let second = 0;
-let IsClicked = false;
-function incrSec(){
-    second = second + 1;
-    var timeSpentString = new String("Total time spent: " + second); 
-    document.getElementById("trackerInside2").innerHTML = timeSpentString;
-}
-function incrementionStopper(){
-    IsClicked = true;
-}
-
-
-while(!IsClicked){
-    setInterval(incrSec, 1000);
+    function countMouseClick() {
+        a = a + 1;
+         document.getElementById("trackerInside").innerHTML =
+         "Number of mouse clicks:" + a;
+    }
+    
+    const elem = document.getElementById("body1");
+    elem.addEventListener("click", countMouseClick);
+    
+    
+    let second = 0;
+    function incrSec(){
+        second = second + 1;
+        var timeSpentString = new String("Total time spent: " + second); 
+        document.getElementById("trackerInside2").innerHTML = timeSpentString;
+        setTimeout(incrSec, 1000);
+    }
+    
+    
+    
 }
